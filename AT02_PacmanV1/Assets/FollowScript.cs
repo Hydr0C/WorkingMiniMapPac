2f108004488d9440ce95a70b6c38df247e5d9569
Hydr0C <sparklesthepteranadon@hotmail.com>
2022-10-31 12:56:21 +0800

    death works
    ghst and pac

--- AT02_PacmanV1/Assets/FollowScript.cs
+++ AT02_PacmanV1/Assets/FollowScript.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class FollowScript : MonoBehaviour
 {
-    public Transform player; 
+    public Transform player; //creates a slot to put in an object
 
     void LateUpdate()
     {
-        Vector3 newPosition = player.position; 
-        newPosition.y = transform.position.y;
-        transform.position = newPosition;
+        Vector3 newPosition = player.position; // finds the player's position
+        newPosition.y = transform.position.y; // updates the variables to the player's new position
+        transform.position = newPosition; // moves the camera to the new position
 
     }
 }
